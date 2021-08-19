e02fe9b06c26e39e875ce048c40502a6f102f1cd
mahro <djamelmahrougui@gmail.com>
2021-08-19 10:13:27 +0100

    add type views

--- server/backend/base/urls/comment_urls.py
+++ server/backend/base/urls/comment_urls.py
@@ -5,10 +5,10 @@ from base.views import comment_views as views
 urlpatterns = [
     path("register/", views.createComment, name="create-comment"),
     path("update/<str:id>/", views.updateComments, name="update-comment"),
-    path("compaign/<str:compaignID>/", views.getCompaignComments, name="compaign_comments"),
-    path("user/<str:userID>/", views.getUserComments, name="user_comments"),
+    path("compaign?<str:compaignID>/", views.getCompaignComments, name="compaign_comments"),
+    path("user?<str:userID>/", views.getUserComments, name="user_comments"),
     path(
-        "compaign/<str:compaignID>/user/<str:userID>/", views.getUserCommentsByCompaign, name="user_compaign_comments"
+        "compaign?<str:compaignID>/user?<str:userID>/", views.getUserCommentsByCompaign, name="user_compaign_comments"
     ),
     path("delete/<str:id>/", views.deleteComment, name="delete_comment"),
 ]
---
+++ server/backend/base/views/type_views.py
@@ -0,0 +1,61 @@
+from base.models import Compaign, Type
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAdminUser, IsAuthenticated
+from base.serializers import TypeSerializer
+from rest_framework.response import Response
+
+## getters ------------------------
+@api_view(["GET"])
+def getTypes(request):
+    types = Type.objects.all()
+    serializer = TypeSerializer(types, many=True)
+    return Response(serializer.data)
+
+
+@api_view(["GET"])
+def getType(request, id):
+    type = Type.objects.get(id=id)
+    serializer = TypeSerializer(type, many=False)
+    return Response(serializer.data)
+
+
+@api_view(["GET"])
+@permission_classes([IsAuthenticated])
+def getCompaignType(request, compaignID):
+    compaign = Compaign.objects.get(id=compaignID)
+    type = Type.objects.filter(compaign=compaign)
+    serializer = TypeSerializer(type, many=False)
+    return Response(serializer.data)
+
+
+## comment creator -----------------
+@api_view(["POST"])
+@permission_classes([IsAdminUser])
+def createType(request):
+    data = request.data
+    type = Type.objects.create(name=data["name"], description=data["description"])
+    serializer = TypeSerializer(type, many=False)
+    return Response(serializer.data)
+
+
+## comment updator -----------------
+@api_view(["PUT"])
+@permission_classes([IsAdminUser])
+def updateTypes(request, id):
+    data = request.data
+    type = Type.objects.get(id=id)
+    type.name = data["name"]
+    type.description = data["description"]
+    type.save()
+
+    serializer = TypeSerializer(type, many=False)
+    return Response(serializer.data)
+
+
+## comment delete view -----------------
+@api_view(["DELETE"])
+@permission_classes([IsAdminUser])
+def deleteType(request, id):
+    type = Type.objects.get(id=id)
+    type.delete()
+    return Response("Type Deleted")
