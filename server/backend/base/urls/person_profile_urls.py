5e95dde99f645bc85afca836cc178cf5778866ea
mahro <djamelmahrougui@gmail.com>
2021-08-20 11:03:43 +0100

    define person profile urls

--- server/backend/base/urls/person_profile_urls.py
+++ server/backend/base/urls/person_profile_urls.py
@@ -2,4 +2,10 @@ from django.urls import path
 from base.views import person_profile_views as views
 
 
-urlpatterns = []
+urlpatterns = [
+    path("register/", views.createPersonProfile, name="create-person-profile"),
+    path("update/<str:id>/", views.updatePersonProfile, name="update-person-profile"),
+    path("<str:id>/", views.getPersonProfile, name="get_person_profile_by_id"),
+    path("", views.getPersonProfiles, name="get_persons"),
+    path("delete/<str:id>/", views.deletePersonProfile, name="delete_person_profile"),
+]
