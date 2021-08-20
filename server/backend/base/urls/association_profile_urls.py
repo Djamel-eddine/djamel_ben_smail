bf499f7fc564942f7cf369eb2d733d10158855d8
mahro <djamelmahrougui@gmail.com>
2021-08-20 11:12:46 +0100

    add association profile views and urls

--- server/backend/base/urls/association_profile_urls.py
+++ server/backend/base/urls/association_profile_urls.py
@@ -2,4 +2,10 @@ from django.urls import path
 from base.views import association_profile_views as views
 
 
-urlpatterns = []
+urlpatterns = [
+    path("register/", views.createAssociationProfile, name="create-association-profile"),
+    path("update/<str:id>/", views.updateAssociationProfile, name="update-association-profile"),
+    path("<str:id>/", views.getAssociationProfile, name="get_association_profile_by_id"),
+    path("", views.getAssociationsProfiles, name="get_associations"),
+    path("delete/<str:id>/", views.deleteAssociationProfile, name="delete_association_profile"),
+]
---
+++ server/backend/base/views/association_profile_views.py
@@ -0,0 +1,80 @@
+from base.models import AssociationProfile, Wilaya
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAdminUser, IsAuthenticated
+from base.serializers import AssociationProfileSerializer
+from rest_framework.response import Response
+from django.contrib.auth.models import User
+
+## getters ------------------------
+@api_view(["GET"])
+@permission_classes([IsAdminUser])
+def getAssociationsProfiles(request):
+    profiles = AssociationProfile.objects.all()
+    serializer = AssociationProfileSerializer(profiles, many=True)
+    return Response(serializer.data)
+
+
+@api_view(["GET"])
+def getAssociationProfile(request, id):
+    user = User.objects.get(id=id)
+    profile = AssociationProfile.objects.get(user=user)
+    serializer = AssociationProfileSerializer(profile, many=False)
+    return Response(serializer.data)
+
+
+## comment creator -----------------
+@api_view(["POST"])
+@permission_classes([IsAuthenticated])
+def createAssociationProfile(request):
+    data = request.data
+    user = User.objects.get(id=data["user"])
+    wilaya = Wilaya.objects.get(id=data["wilaya"])
+    profile = AssociationProfile.objects.create(
+        user=user,
+        phone=data["phone"],
+        fax=data["fax"],
+        name=data["name"],
+        activity=data["activity"],
+        associationNumber=data["associationNumber"],
+        logo=request.FILES.get("logo"),
+        coverImage=request.FILES.get("coverImage"),
+        baseWilaya=wilaya,
+        facebook=data["facebook"],
+        twitter=data["twitter"],
+    )
+    serializer = AssociationProfileSerializer(profile, many=False)
+    return Response(serializer.data)
+
+
+## comment updator -----------------
+@api_view(["PUT"])
+@permission_classes([IsAdminUser])
+def updateAssociationProfile(request, id):
+    data = request.data
+    user = User.objects.get(id=data["user"])
+    wilaya = Wilaya.objects.get(id=data["wilaya"])
+    profile = AssociationProfile.objects.get(user=user)
+    profile.phone = data["phone"]
+    profile.fax = data["fax"]
+    profile.name = data["name"]
+    profile.activity = data["activity"]
+    profile.associationNumber = data["associationNumber"]
+    profile.logo = request.FILES.get("logo")
+    profile.coverImage = request.FILES.get("coverImage")
+    profile.baseWilaya = wilaya
+    profile.facebook = data["facebook"]
+    profile.twitter = data["twitter"]
+    profile.save()
+
+    serializer = AssociationProfileSerializer(type, many=False)
+    return Response(serializer.data)
+
+
+## comment delete view -----------------
+@api_view(["DELETE"])
+@permission_classes([IsAdminUser])
+def deleteAssociationProfile(request, id):
+    user = User.objects.get(id=id)
+    profile = AssociationProfile.objects.get(user=user)
+    profile.delete()
+    return Response("Type Deleted")
